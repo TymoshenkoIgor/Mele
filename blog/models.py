af6e6789a46f5dc3dd65696d6483cf0ce3a0606a
Goracio84 <goracio84@gmail.com>
2018-05-02 19:30:05 +0300

    Add html and forms

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone 
 from django.contrib.auth.models import User
+from  django.core.urlresolvers import reverse
 
 
 class PublishedManager(models.Manager):
@@ -9,27 +10,33 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
-	STATUS_CHOICE=(
-			('draft', 'Draft'),
-			('published', 'Published'),
-		)
-	title = models.CharField(max_length=250)
-	slug = models.SlugField(max_length=250, unique_for_date='publish')
-	author = models.ForeignKey(User, related_name='blog_posts')
-	body = models.TextField()
-	publish = models.DateTimeField(default=timezone.now)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='draft')
-    objects = models.Manager()
-    published = PublishedManager()
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # The Dahl-specific manager.
 
-	class Meta:
-		ordering = ('-publish',)
+    class Meta:
+        ordering = ('-publish',)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts':posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page not an Integer deliver the first page
+        posts = paginator.page(1)
+    except  EmptyPage:
+        # If page is out of the range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page':page, 'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -12,3 +24,11 @@ def post_detail(request, year, month, day, post):
                                     publish__month=month,
                                     publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
