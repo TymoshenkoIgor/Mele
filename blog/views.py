f293d17ae740254e4ab73b2ab791e38da00e5b8d
Goracio84 <goracio84@gmail.com>
2018-05-28 15:56:25 +0300

    Continue

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def post_list(request, tag_slug=None):
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list=object_list.filter(tags__in=[tag])
-        
+
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -24,7 +24,7 @@ def post_list(request, tag_slug=None):
     except  EmptyPage:
         # If page is out of the range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page':page, 'posts':posts})
+    return render(request, 'blog/post/list.html', {'page':page, 'posts':posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
