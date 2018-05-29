ac3f02ff34ebc1353452fbbf1a4e865c32b69ac3
IgTym <IgTym@bitbucket.org>
2018-05-29 13:09:02 +0300

    Tamplate tag

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView
 from blog.forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
+
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -25,6 +27,7 @@ def post_list(request, tag_slug=None):
         # If page is out of the range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'page':page, 'posts':posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -47,7 +50,11 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
         else:
             comment_form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form, 'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
